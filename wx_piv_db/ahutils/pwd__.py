f36b71455ac8302d1588d2e7c9c0cb0637c0ad6d
Alexander <a.hetland@web.de>
2014-09-02 11:12:55 +0200

    x

--- wx_piv_db/ahutils/pwd__.py
+++ wx_piv_db/ahutils/pwd__.py
@@ -10,11 +10,10 @@
 #===============================================================================
 import os
 
-
 def pwd(windowsUser):
     """Returns the compiled password if the user name matches the windows log-inn."""
     if windowsUser==os.environ.get( "USERNAME" ):
-        return 'sdfjhio8974sadyxc'
+        return 'sz'
     else:
         return None
 
@@ -22,7 +21,32 @@ def createFile():
     "Compiple the file. Rename the file first though!!!"
     import py_compile
     py_compile.compile('pwd.py')
-     
+
+
+
+import uuid
+import hashlib
+ 
+def hash_password(password):
+    # uuid is used to generate a random number
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+    
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+def test():
+    new_pass = raw_input('Please enter a password: ')
+    hashed_password = hash_password(new_pass)
+    print('The string to store in the db is: ' + hashed_password)
+    old_pass = raw_input('Now please enter the password again to check: ')
+    if check_password(hashed_password, old_pass):
+        print('You entered the right password')
+    else:
+        print('I am sorry but the password does not match')
+    
+  
 if __name__=='__main__':
-    createFile()
-    pass
+    #createFile()
+    test()
