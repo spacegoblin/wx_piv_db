90378b26a82fbfe4916ee26c56123e254b3054a3
Alexander <a.hetland@web.de>
2014-12-02 10:56:22 +0100

    Update loadDatev.py
    changed to fit German windows settings

--- wx_piv_db/scripts/loadDatev.py
+++ wx_piv_db/scripts/loadDatev.py
@@ -22,7 +22,7 @@ db.setQuote('Postgress')
 
 
 
-DB = db.This_Db(const.db_lst_dsn, 'ahetland', pwd.pwd('hetland'))
+DB = db.This_Db(const.db_lst_dsn, 'ahetland', pwd.pwd('alhet'))
 
 #BL    Kontonummer    Datum    BU    Gegenkonto    Buchungstext    USt%    Belegfeld1    Umsatz Soll    Umsatz Haben    WKZ    Eingabebetrag    Kurs    Stapel-Nr.    BSNr.    HK    KOST1    KOST2    KOST-Menge    ZI
 
@@ -42,65 +42,65 @@ def helpMonth(stapelNr):
 def run(FILE):
     """Insert a datev file into the database"""
                     
-    reader = csv.reader(open(FILE, "rb"), delimiter=",")
+    reader = csv.reader(open(FILE, "rb"), delimiter=";")
     i=0
     lst = []
-    try:
-        for row in reader:
-            dict_ = {}
-            if i==0:
-                try:
-                    assert row==CSV_ROW, "%s" % row
-                except AssertionError:
-                    print row
-                    raise
-                i+=1
-            else:
-                dict_['account_datev'] = row[1]
-                dict_['datum'] = helper( row[2] )
-                dict_['bu'] = row[3]
-                dict_['gegenkonto'] = row[4]
+   # try:
+    for row in reader:
+        dict_ = {}
+        if i==0:
+            try:
+                assert row==CSV_ROW, "%s" % row
+            except AssertionError:
+                print row
+                raise
+            i+=1
+        else:
+            dict_['account_datev'] = row[1]
+            dict_['datum'] = helper( row[2] )
+            dict_['bu'] = row[3]
+            dict_['gegenkonto'] = row[4]
 
-                dict_['buchungstext'] = unicode(row[5], errors='ignore')  #.decode('utf-8').encode('utf-8')
-                dict_['ust'] = row[6]
-                dict_['belegfeld1'] = row[7].strip()
-                dict_['soll'] = row[8].replace(',', '')
-                dict_['haben'] = row[9].replace(',', '')
-                dict_['wkz'] = row[10]
-                #dict_['xx'] = row[11]
-                dict_['kurs'] = row[12].strip()
-                dict_['stapel_nr'] = row[13].strip()
-                dict_['bsnr'] = row[14]
-                dict_['hk'] = row[15]
-                dict_['kost1'] = row[16]
-                
-                dict_['period'] = helpMonth( dict_['stapel_nr'] )
-                dict_['company'] = 'LSE HGB'
-                dict_['imp_str'] = const.random_str
-                
-                dict_['zui'] = const.random_str
-                
-                sql = db.dictToInsert(dict_, 'tbl_susa')
+            dict_['buchungstext'] = unicode(row[5], errors='ignore')  #.decode('utf-8').encode('utf-8')
+            dict_['ust'] =  row[6].replace(',', '.')
+            dict_['belegfeld1'] = row[7].strip()
+            dict_['soll'] = row[8].replace('.', '').replace(',', '.')
+            dict_['haben'] = row[9].replace('.', '').replace(',', '.')
+            dict_['wkz'] = row[10]
+            #dict_['xx'] = row[11]
+            dict_['kurs'] = row[12].replace(',', '.')
+            dict_['stapel_nr'] = row[13].strip()
+            dict_['bsnr'] = row[14]
+            dict_['hk'] = row[15]
+            dict_['kost1'] = row[16]
+            
+            dict_['period'] = helpMonth( dict_['stapel_nr'] )
+            dict_['company'] = 'LSE HGB'
+            dict_['imp_str'] = unicode(const.random_str, errors='ignore')
+            
+            dict_['zui'] = unicode(const.random_str, errors='ignore')
+            
+            sql = db.dictToInsert(dict_, 'tbl_susa')
 
-                print sql
-                
-                DB.c.execute(sql)
-                DB.cnn.commit()
-                
-                i+=1        
+            print sql
+            
+            DB.c.execute(sql)
+            DB.cnn.commit()
+            
+            i+=1        
 
-    except:
-        print 'line %d: %s' % (reader.line_num, sys.exc_info()[0] )
-    
-    finally:
-        print "DONE! %d records inserted" % (i-1)
+##    except:
+##        print 'line %d: %s' % (reader.line_num, sys.exc_info()[0] )
+##    
+##    finally:
+##        print "DONE! %d records inserted" % (i-1)
 
 
 
 if __name__ == '__main__':
    
    #T:\Reporting\2014\08-Aug\LSE
-    PATH='T:\\Reporting\\2014\\10-Oct\\LSE\\v02\\imp.csv'
+    PATH='L:\\Reporting\\2014\\11-Nov\\LSE\\imp.csv'
 
     run(PATH)
 
